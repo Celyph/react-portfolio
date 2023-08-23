aa33a8c37b0890c7e2559a868b70f40b52dcfc59
Celyph <briannaslicer@gmail.com>
2023-08-23 12:00:45 -0500

    Added information and images to the pages

--- Develop/challenge20/src/App.jsx
+++ Develop/challenge20/src/App.jsx
@@ -28,13 +28,10 @@ function Project({name, desc, imgurl}) {
 
 function Projects() {
   const projects = [
-    {name: 'asdf', desc: 'some stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
-    {name: 'jkl;', desc: 'more stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
-    {name: 'balls', desc: 'more stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
-    {name: 'eeee', desc: 'more stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
-    {name: 'jkl;', desc: 'more stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
-    {name: 'jkl;', desc: 'more stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
-    {name: 'ooo', desc: 'more stuff', img: 'https://i0.wp.com/chemmatcars.uchicago.edu/wp-content/uploads/2021/03/default-placeholder-image-1024x1024-1.png?ssl=1'},
+    {name: 'Pokemon Quiz', desc: 'A simple quiz about Pokemon that utilized JS to have a coundown timer that subtracts time if a question is answered incorrectly.', img: 'https://i.imgur.com/IBzdV8p.png'},
+    {name: 'Password Generator;', desc: 'A password generator using prompts to ask for each desired aspect of said password.', img: 'https://i.imgur.com/EhRNdrd.png'},
+    {name: 'Task Scheduler', desc: 'A simple task scheduler that stores user input to localstorage and displays past, current, and present times using color coding.', img: 'https://i.imgur.com/9jqJmCE.png'},
+    {name: 'First Portfolio', desc: 'This is the very first portfolio, and first challenge, I made in my coding bootcamp using HTML and CSS.', img: 'https://i.imgur.com/2XgNbcd.png'}
   ]
 
   return (
@@ -45,7 +42,7 @@ function Projects() {
 }
 
 function About() {
-  const aboutMe = 'some stuff about me'
+  const aboutMe = 'MY NAME IS BRIANNA SLICER AND I AM CURRENTLY A STUDENT IN THE KU CODING BOOTCAMP, AND TECHNICAL SUPPORT FOR POSIM. I JOINED THE BOOTCAMP TO FURTHER DEVELOP MY SKILLS REGARDING CODING AND BUILD A PORTFOLIO FOR FUTURE OPPORTUNITIES. I MOST ENJOY UTILIZING HTML AND CSS TO CREATE VISUALLY PLEASING AND FUNCTIONAL WEBPAGES. MY EXPERIENCE AS A TECHNICAL SUPPORT TEAM MEMBER WITH POSIM HAS HELPED ME GAIN INFORMATION ON COMMAND LINE, MYSQL, AND HOW TO OPERATE BOTH MAC AND WINDOWS COMPUTERS. OUTSIDE OF THE BOOTCAMP AND POSIM, I ENJOY SPENDING TIME WITH MY CATS, PLAYING VIDEO GAMES AND PAINTING.'
   return (
     <>
       <p>{aboutMe}</p>
